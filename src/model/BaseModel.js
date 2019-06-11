a8d4349e5a58014b546ac55c8afa02e2006a97e2
rehellinen <912377791@qq.com>
2019-06-11 16:41:13 +0800

    model

---
+++ src/model/BaseModel.js
@@ -0,0 +1,10 @@
+/**
+ *  BaseModel.js
+ *  Create By rehellinen
+ *  Create On 2019/6/11 16:18
+ */
+
+import {Model} from "../libs/model/Model"
+
+export class BaseModel extends Model{
+}
---
+++ src/model/TokenModel.js
@@ -0,0 +1,56 @@
+/**
+ *  TokenModel.js
+ *  Create By rehellinen
+ *  Create On 2018/9/25 23:12
+ */
+import {BaseModel} from './BaseModel'
+import {DatabaseException} from "../common/exception/DatabaseException"
+
+let {TOKEN_TYPE} = $config
+
+export class TokenModel extends BaseModel{
+  constructor () {
+    super({
+      tableName: 'token'
+    })
+  }
+
+  /**
+   * 获取数据库中最新的access_token
+   * @param type access_token / ticket
+   */
+  async getToken (type = TOKEN_TYPE.ACCESS_TOKEN) {
+    let data =  await this.model
+      .where({type})
+      .orderBy('id', 'DESC')
+      .fetch()
+    if (!data) {
+      throw new DatabaseException()
+    }
+
+    return data.attributes
+  }
+
+  /**
+   * 保存access_token到数据库
+   * @param data
+   * @param type access_token / ticket
+   * @return {Promise<boolean>}
+   */
+  async saveToken (data, type = TOKEN_TYPE.ACCESS_TOKEN) {
+    let savedData = {
+      token: data.access_token || data.ticket,
+      expires_in: data.expires_in,
+      type: type
+    }
+
+    let res = await new this.model(savedData)
+      .save(null, {method: 'insert'})
+    if (!res) {
+      throw new DatabaseException({
+        status: 40001,
+        message: '插入Token数据失败'
+      })
+    }
+  }
+}
